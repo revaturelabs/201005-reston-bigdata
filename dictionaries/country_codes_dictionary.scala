668c96b6b82cb9859d0144935305b10309ea7e71
Sean <sean.w.horner@gmail.com>
2020-11-25 14:31:09 -0600

    Landlocked countries list and country code dictionary (started).

--- dictionaries/country_codes_dictionary.scala
+++ dictionaries/country_codes_dictionary.scala
@@ -1,1 +1,6 @@
-// This dictionary contains the 
+// This dictionary contains the country codes (2-character and 3-character) for each country.
+// Pulled from: https://www.iban.com/country-codes
+
+val country_codes = Map[String,(String, String)](
+
+)
