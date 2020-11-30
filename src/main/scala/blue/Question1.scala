a2f9ad4066478b4493b4a19a7ee27e4d79314b55
LiamAHood <liam.hood@revature.net>
2020-11-30 12:07:19 -0800

    updated question 1 to use latest case class structure

--- src/main/scala/blue/Question1.scala
+++ src/main/scala/blue/Question1.scala
@@ -1,9 +1,15 @@
 package blue
 
+import org.apache.spark.sql.functions.explode
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Question1 {
   def initialSolution(spark: SparkSession, data: DataFrame): Unit ={
+	import spark.implicits._
+    val data = origialData
+      .select($"name", $"agg_population", $"agg_gdp", explode($"agg_case_data"))
+      .select($"name", $"agg_population", $"agg_gdp" as "GDP", $"col.date" as "date", $"col.new_cases" as "new_cases")
+	  
     val regionByInfectionRateFull = rankRegionsByMetric.calculateMetric(spark, data, "new_cases", "agg_population",
       100000, "infections_per_pop_100k")
 
