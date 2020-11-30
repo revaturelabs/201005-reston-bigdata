795ea44922043d186da62bf8f048b49e6acb52c5
LiamAHood <liam.hood@revature.net>
2020-11-30 14:15:11 -0800

    added question 8 part 2

--- pj3-bigdata/src/main/scala/blue/Question8.scala
+++ pj3-bigdata/src/main/scala/blue/Question8.scala
@@ -33,7 +33,58 @@ object Question8 {
     }
   }
 
-  def regionFirstPeak(spark: SparkSession, df: DataFrame): Unit= {
+  def regionFirstPeak(spark: SparkSession, dfRegion: DataFrame, dfCountry: DataFrame): Unit= {
+    import spark.implicits._
+    val dataInit = dfRegion
+      .select($"name", $"countries", explode($"agg_case_data"))
+
+    val data = dataInit
+      .select($"name", $"col.date" as "date", $"col.total_cases_per_million" as "total_cases")
+
+    val countryData = dfCountry
+      .select($"name", explode($"case_data"))
+      .select($"name", $"col.date" as "date", $"col.total_cases_per_million" as "total_cases")
+
+    val regionList = data.select("name").distinct().collect().map(_.getString(0))
+
+    var tempDates: Array[Double] = null
+    var tempCases: Array[Double] = null
+    var tempFrame: DataFrame = null
+
+    val firstPeakTime: ArrayBuffer[Double] = ArrayBuffer()
+    for (region <- regionList) {
+      tempFrame = data.where($"name" === region)
+      tempCases = tempFrame.select($"total_cases").collect().map(_.getDouble(0))
+      tempDates = tempFrame.select($"date").collect().map(_.getDouble(0))
+      firstPeakTime.append(StatFunc.firstPeak(tempDates, tempCases, 7, 1)._1)
+    }
+
+    val firstPeakTimeAvg: ArrayBuffer[Double] = ArrayBuffer()
+    val firstPeakForCountry: ArrayBuffer[Double] = ArrayBuffer()
+    var countryList: Array[Row] = Array()
+    for (region <- regionList) {
+      countryList = dataInit
+        .select($"countries")
+        .where($"name" === region)
+        .collect()
+      for (country <- countryList){
+        tempFrame = countryData.where($"name" === country)
+        tempCases = tempFrame.select($"total_cases").collect().map(_.getDouble(0))
+        tempDates = tempFrame.select($"dates").collect().map(_.getDouble(0))
+        firstPeakForCountry.append(StatFunc.firstPeak(tempDates, tempCases, 7, 1)._1)
+      }
+      firstPeakTimeAvg.append(firstPeakForCountry.sum/firstPeakForCountry.length)
+      firstPeakForCountry.clear()
+    }
+
+    val firstPeakTable: ArrayBuffer[(String, Double, Double)] = ArrayBuffer()
+    for (ii <- 0 to regionList.length-1){
+      firstPeakTable.append((regionList(ii), firstPeakTime(ii), firstPeakTimeAvg(ii)))
+    }
+    println("")
+    for (ii <- 0 to regionList.length-1){
+      println(s"${firstPeakTable(ii)._1},\t${firstPeakTable(ii)._2},\t${firstPeakTable(ii)._3}" )
+    }
 
   }
 }
