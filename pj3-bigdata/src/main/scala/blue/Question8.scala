c67aeb5ab5a8546e7d76833628fadbc83bc33c83
srtidd23 <sean.tidd@revature.net>
2020-11-30 16:38:13 -0500

    Part 1 Q8 done for now

--- pj3-bigdata/src/main/scala/blue/Question8.scala
+++ pj3-bigdata/src/main/scala/blue/Question8.scala
@@ -1,5 +1,6 @@
 package blue
 
+import org.apache.spark.sql.types.DoubleType
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 import scala.collection.mutable.ArrayBuffer
@@ -9,10 +10,26 @@ object Question8 {
   def regionCorrelation(spark: SparkSession, df: DataFrame): Unit={
     import spark.implicits._
 
-    var regionNames = ArrayBuffer[String]()
-    df.select("name").distinct().rdd.map(region => regionNames += region.get(0).toString)
-    for(region <- 0 to regionNames.length-1){
+    //Grab all available region names
+    val regionNames = df.select("name").sort("name").distinct().rdd.map(_.get(0).toString).collect()
 
+    for(region <- 0 to regionNames.length-1){
+      //sort DataFrame according to GDP and filter by each region
+      val regionArray = df.select("name", "agg_gdp", "agg_cases").
+        filter($"name" === regionNames(region))
+        .withColumn("agg_gdp", $"agg_gdp".cast(DoubleType))
+        .withColumn("agg_cases", $"agg_cases".cast(DoubleType))
+        .orderBy($"agg_gdp").rdd.collect()
+      //Collect the x and y data from region and prepare it for graph/stats
+      var gdpData = ArrayBuffer[Double]()
+      var casesData = ArrayBuffer[Double]()
+      for(i <- 0 to regionArray.length-1){
+        gdpData += regionArray(i).get(1).toString.toDouble
+        casesData += regionArray(i).get(2).toString.toDouble
+      }
+      //get the regions correlation
+      val correlation = StatFunc.correlation(gdpData.toArray, casesData.toArray)
+      println(s"Region ${regionNames(region)}'s GDP-Infection rate correlation: ${correlation}'")
     }
   }
 
--- pj3-bigdata/src/main/scala/runner/Runner.scala
+++ pj3-bigdata/src/main/scala/runner/Runner.scala
@@ -1,11 +1,23 @@
 package runner
+import blue.Question8
+import org.apache.spark.sql.SparkSession
+
 
 object Runner {
   def main(args: Array[String]): Unit = {
     // Command Line Interface format that accepts params
     // Have a use case for your question below
+    val spark = SparkSession.builder()
+      .master("local[4]")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val df = spark.read.option("header",true).csv("example.csv")
+    df.show()
+
     args match {
-      case _ => println("Hello World")
+      case _ => Question8.regionCorrelation(spark, df)//println("Hello World")
     }
   }
 }
