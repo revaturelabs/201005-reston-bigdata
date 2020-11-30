0bf4344c4c4e89e587b864065b93c34cb981aca2
srtidd23 <sean.tidd@revature.net>
2020-11-30 12:47:03 -0500

    added more graph functions and Question8

---
+++ src/main/scala/blue/Question8.scala
@@ -0,0 +1,14 @@
+package blue
+
+import org.apache.spark.sql.DataFrame
+
+object Question8 {
+
+  def regionCorrelation(df: DataFrame): Unit={
+
+  }
+
+  def regionFirstPeak(df: DataFrame): Unit= {
+
+  }
+}
