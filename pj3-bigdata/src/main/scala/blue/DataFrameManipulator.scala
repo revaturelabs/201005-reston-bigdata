af487e58b87be0fee32da5da928f9b2c56694e16
LiamAHood <liam.hood@revature.net>
2020-12-01 13:19:34 -0800

    updated casejoin

--- pj3-bigdata/src/main/scala/blue/DataFrameManipulator.scala
+++ pj3-bigdata/src/main/scala/blue/DataFrameManipulator.scala
@@ -5,14 +5,15 @@ import org.apache.spark.sql.functions.explode
 
 object DataFrameManipulator {
   def caseJoin(spark: SparkSession, regionDF: DataFrame, caseDF: DataFrame): DataFrame ={
-//    import spark.implicits._
-//    val regionDict = regionDF
-//      .select($"name", explode($"countries") as "country")
-//      .select($"name" as "region", $"country.name" as "country")
-//
-//    val data = caseDF
-//      .select($"name", $"country"$"total_cases", $"new_cases")
+    import spark.implicits._
 
+    val regionDict = regionDF
+      .select($"name", explode($"countries") as "country")
+      .select($"name" as "region", $"agg_population", $"country.name" as "country")
+
+    caseDF
+      .select( $"date", $"country", $"total_cases", $"new_cases")
+      .join(regionDict, $"country")
 
   }
 
