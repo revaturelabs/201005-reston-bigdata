bbb7e48192cf3acbca5d2033570179f6e03211b5
Christopher Chee <cchee1@lsu.edu>
2020-12-01 16:18:23 -0600

    updated to calculate infection rate using daily stats now

--- src/main/scala/orange/orangeRunner.scala
+++ src/main/scala/orange/orangeRunner.scala
@@ -13,14 +13,34 @@ object orangeRunner {
     import spark.implicits._
     spark.sparkContext.setLogLevel("WARN")
 
-    //import the covid data from testData file as well as the border data from the provided border data
-    val country_data = spark.read.option("multiline", "true").json("testData")
+    //imports country data from countries_general_stats.tsv to obtain population for each country
+    val country_stats = spark.read.option("multiline", "true").option("header", "true").option("sep", "\t").csv("countries_general_stats.tsv")
+    // imports daily COVID data for each country from daily_stats.tsv
+    val country_data = spark.read.option("multiline", "true").option("header", "true").option("sep", "\t").csv("daily_stats.tsv")
+
     //TODO:need to cross country_codes_a2 with itself to make a list of all potential countries that can border(many of them wont) this will work just like borders does, but using in house code
     val borders = spark.read.option("header", "true").csv("border_data.csv")
-    borders.show(100)
+
+    // selects country and casts population to integer country data
+    val country_pop = country_stats.select($"COUNTRY", $"POPULATION".cast("Int"))
+    // selects maximum of total cases for each country from COVID data
+    val country_cases = country_data
+      .filter($"TOTAL_CASES" =!= "NULL")
+      .withColumn("Cases", $"TOTAL_CASES".cast("Int"))
+      .select($"COUNTRY", $"Cases".as("Total Cases"))
+      .groupBy($"COUNTRY")
+      .agg(functions.max($"Total Cases").as("TOTAL CASES"))
+      .sort(functions.asc("COUNTRY"))
+
+    // joins population data with COVID data and calculates infection rate per capita
+    val infection_rate = country_cases
+      .join(country_pop, country_cases("COUNTRY") === country_pop("COUNTRY"))
+      .select(country_cases("COUNTRY"), $"TOTAL CASES", $"POPULATION",
+        ($"TOTAL CASES" * 100 / $"POPULATION").as("INFECTION RATE PER CAPITA"))
+      .sort(functions.asc("COUNTRY"))
 
           //create "infection_rate" from covid data directory with daily covid data
-        val infection_rate = country_data
+        /*val infection_rate = country_data
           .select($"location", $"population", functions.explode($"data")) //.explode will separate our nested data so we can access it
           .select($"location", $"population", $"col.date", $"col.total_cases")
           .groupBy("location", "population")
@@ -32,7 +52,7 @@ object orangeRunner {
           .sort(functions.desc("infection_rate_per_capita"))
           .withColumn("infection_rate_per_capita", 'infection_rate_per_capita.cast("Decimal(5,3)")) //make values easier to read
 //          .withColumn("population", 'population.cast("Decimal(10,0)"))
-//          .withColumn("total_cases", 'total_cases.cast("Decimal(12,0)"))
+//          .withColumn("total_cases", 'total_cases.cast("Decimal(12,0)"))*/
 
 //TODO: once we have updated borders with inhouse code, need to adapt bcountries to use borders_dictionary as opposed to border_data.csv
     /*
@@ -40,9 +60,9 @@ object orangeRunner {
     to give us all the countries that border a country(home country) and the infection rate of the home country. The infection rate of the bordering country
     will be retrieved in the next join
      */
-    val bcountries = borders.join(infection_rate, borders("country_name") === infection_rate("location"), "inner")
+    val bcountries = borders.join(infection_rate, borders("country_name") === infection_rate("COUNTRY"), "inner")
       .select(borders("country_name"), borders("country_code"), borders("country_border_name"),
-        borders("country_border_code"), infection_rate("infection_rate_per_capita").as("home_country_infection_rate_per_capita"))
+        borders("country_border_code"), infection_rate("INFECTION RATE PER CAPITA").as("home_country_infection_rate_per_capita"))
 //    bcountries.show(10)
     //first.printSchema()
 
@@ -51,13 +71,13 @@ object orangeRunner {
     the result of joining our border countries with our infection data. This gives us information about the home country, its infection rate,
     bordering countries and their infection rate, and a delta value, which is the difference between these 2 rates.
      */
-    val res1 = bcountries.join(infection_rate, bcountries("country_border_name") === infection_rate("location"), "inner")
+    val res1 = bcountries.join(infection_rate, bcountries("country_border_name") === infection_rate("COUNTRY"), "inner")
       .select(bcountries("country_name"), bcountries("country_code"), bcountries("home_country_infection_rate_per_capita").as("home_country_infection_rate_per_capita(%)"),
         bcountries("country_border_name"), bcountries("country_border_code"),
-        infection_rate("infection_rate_per_capita").as("country_border_infection_rate(%)"),
+        infection_rate("INFECTION RATE PER CAPITA").as("country_border_infection_rate(%)"),
         //used to calculate the delta value. round() allows us to do a calculation and round the number of decimal places, making it easier to read
 //        round((bcountries("home_country_infection_rate_per_capita") - infection_rate("infection_rate_per_capita")), 2).as("delta"))
-        (bcountries("home_country_infection_rate_per_capita") - infection_rate("infection_rate_per_capita")).as("delta"))
+        (bcountries("home_country_infection_rate_per_capita") - infection_rate("INFECTION RATE PER CAPITA")).as("delta"))
         //res.show(10)
 
     /*
@@ -71,14 +91,14 @@ object orangeRunner {
 //    waterLocked.show(10)
 
     //combine landLocked dataframe with infection_rate to make a dataframe with the infection rate of land locked countries
-    val landLockedInfRate = landLocked.join(infection_rate, landLocked("country_name") === infection_rate("location"), "inner")
+    val landLockedInfRate = landLocked.join(infection_rate, landLocked("country_name") === infection_rate("COUNTRY"), "inner")
       .select(landLocked("country_name"), landLocked("country_code"),
-        infection_rate("infection_rate_per_capita").as("infection_rate_per_capita(%)"))
+        infection_rate("INFECTION RATE PER CAPITA").as("infection_rate_per_capita(%)"))
 
     //combine waterLocked dataframe with infection_rate to make a dataframe with the infection rate of water locked countries
-    val waterLockedInfRate = waterLocked.join(infection_rate, waterLocked("country_name") === infection_rate("location"), "inner")
+    val waterLockedInfRate = waterLocked.join(infection_rate, waterLocked("country_name") === infection_rate("COUNTRY"), "inner")
       .select(waterLocked("country_name"), waterLocked("country_code"),
-        infection_rate("infection_rate_per_capita").as("infection_rate_per_capita(%)"))
+        infection_rate("INFECTION RATE PER CAPITA").as("infection_rate_per_capita(%)"))
 //    waterLockedInfRate.show(10)
 
 
