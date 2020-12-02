7b697ff428568da2a3ac43d6e58bb1d6103fcfa7
LiamAHood <liam.hood@revature.net>
2020-12-02 08:59:23 -0800

    made it runnable I think

--- pj3-bigdata/src/main/scala/blue/BlueRunner.scala
+++ pj3-bigdata/src/main/scala/blue/BlueRunner.scala
@@ -7,10 +7,14 @@ object BlueRunner extends App {
     .master("local[4]")
     .getOrCreate()
 
-  val econRawDF = spark.read.option("delimiter","\t").option("header",true).csv("C:/Users/river/IdeaProjects/201005-reston-bigdata/WorldEconomicData_AllCountries_Test.tsv")
-  val caseRawDF = spark.read.csv("C")
+//  val econRawDF = spark.read.option("delimiter","\t").option("header",true).csv("C:/Users/river/IdeaProjects/201005-reston-bigdata/WorldEconomicData_AllCountries_Test.tsv")
+//  val caseRawDF = spark.read.csv("C")
+//  val econRawDF = spark.read.option("delimiter","\t").option("header",true)
+//  .csv("s3://adam-king-848/data/WorldEconomicData_AllCountries_Test.tsv")
+  val caseRawDF = spark.read.option("delimiter","\t").option("header",true)
+    .csv("s3://adam-king-848/data/daily_stats.tsv")
 
-  econRawDF.show()
+//  econRawDF.show()
   caseRawDF.show()
 
 println("Hello World")
