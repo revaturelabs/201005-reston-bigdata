ca1ded259e3bad25bd133f55175999150784a966
srtidd23 <sean.tidd@revature.net>
2020-12-02 12:56:02 -0500

    removed key

--- pj3-bigdata/src/main/scala/blue/BlueRunner.scala
+++ pj3-bigdata/src/main/scala/blue/BlueRunner.scala
@@ -5,9 +5,6 @@ import org.apache.spark.sql.SparkSession
 object BlueRunner extends App {
   val spark = SparkSession.builder()
     .master("local[4]")
-    .config("spark.hadoop.fs.s3a.access.key", "AKIA4OK5FKIYQUHKGGEP")
-    .config("spark.hadoop.fs.s3a.secret.key", "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")
-    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
     .getOrCreate()
 
 
