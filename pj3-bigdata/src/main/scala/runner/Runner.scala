06cc827de7f990d88c03406dd3b62aed22e381ad
srtidd23 <sean.tidd@revature.net>
2020-12-02 14:48:17 -0500

    runner update

--- pj3-bigdata/src/main/scala/runner/Runner.scala
+++ pj3-bigdata/src/main/scala/runner/Runner.scala
@@ -1,24 +1,31 @@
-//package runner
-//import blue.Question8
-//import org.apache.spark.sql.SparkSession
-//
-//
-//object Runner {
-//  def main(args: Array[String]): Unit = {
-//    // Command Line Interface format that accepts params
-//    // Have a use case for your question below
-//    val spark = SparkSession.builder()
-//      .master("local[4]")
-//      .getOrCreate()
-//
-//    spark.sparkContext.setLogLevel("ERROR")
-//
-//    val df = spark.read.option("header",true).csv("example.csv")
-//    df.show()
-//
-//    args match {
-//      case _ => Question8.regionCorrelation(spark, df)//println("Hello World")
-//    }
-//  }
-//}
+
+package runner
+//import your package here like so:
+import blue.Question8
+import org.apache.spark.sql.SparkSession
+
+
+object Runner {
+  def main(args: Array[String]): Unit = {
+    // Command Line Interface format that accepts params
+    //Have a use case for your question below
+    val spark = SparkSession.builder()
+      .master("local[4]")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    args match {
+        // Add the corresponding question function like so (Q8 is commented for rework):
+      case Array(func,param1, param2) if(func == "Q8") => //Question8.regionCorrelation(spark, df)
+      case _ => printMenu()
+    }
+  }
+
+  def printMenu(): Unit ={
+    // Add your input format in the help menu below:
+    println("___________________________________Menu___________________________________")
+    println("Q8 <Param 1> <Param 2> | GDP-Peak correlation and avg first peak")
+  }
+}
 
